c55a6e8ed0379bb5a71d90970071c833f756a38c
IsakRegell <Isak.regell@gmail.com>
2025-03-21 15:31:10 +0100

    New controller

---
+++ Onsdagapi/Controllers/CustomersController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Onsdagapi.Models;
+
+namespace Onsdagapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly BankDbContext _context;
+
+        public CustomersController(BankDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetAllCustomers()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomerById(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        // PUT: api/Customers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCustomerById(int id, Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DoesCustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Customers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Customer>> UpdateCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
+        }
+
+        // DELETE: api/Customers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomerById(int id)
+        {
+            var customerToDelete = await _context.Customers.FindAsync(id);
+            if (customerToDelete == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customerToDelete);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DoesCustomerExists(int id)
+        {
+            return _context.Customers.Any(e => e.CustomerId == id);
+        }
+    }
+}
--- Onsdagapi/Controllers/TransactionsController.cs
+++ Onsdagapi/Controllers/TransactionsController.cs
@@ -22,29 +22,29 @@ namespace Onsdagapi.Controllers
 
         // GET: api/Transactions
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Transaction>>> GetTransactions()
+        public async Task<ActionResult<IEnumerable<Transaction>>> GetAllTransactions()
         {
             return await _context.Transactions.ToListAsync();
         }
 
         // GET: api/Transactions/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Transaction>> GetTransaction(int id)
+        public async Task<ActionResult<Transaction>> GetExistingTransactionById(int id)
         {
-            var transaction = await _context.Transactions.FindAsync(id);
+            var existingTransaction = await _context.Transactions.FindAsync(id);
 
-            if (transaction == null)
+            if (existingTransaction == null)
             {
                 return NotFound();
             }
 
-            return transaction;
+            return existingTransaction;
         }
 
         // PUT: api/Transactions/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutTransaction(int id, Transaction transaction)
+        public async Task<IActionResult> UpdateTransaction(int id, Transaction transaction)
         {
             if (id != transaction.TransactionId)
             {
@@ -59,7 +59,7 @@ namespace Onsdagapi.Controllers
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!TransactionExists(id))
+                if (!DoesTransactionExists(id))
                 {
                     return NotFound();
                 }
@@ -75,31 +75,31 @@ namespace Onsdagapi.Controllers
         // POST: api/Transactions
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Transaction>> PostTransaction(Transaction transaction)
+        public async Task<ActionResult<Transaction>> CreateTransaction(Transaction transaction)
         {
             _context.Transactions.Add(transaction);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetTransaction", new { id = transaction.TransactionId }, transaction);
+            return CreatedAtAction("GetExistingTransaction", new { id = transaction.TransactionId }, transaction);
         }
 
         // DELETE: api/Transactions/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTransaction(int id)
+        public async Task<IActionResult> DeleteTransactionById(int id)
         {
-            var transaction = await _context.Transactions.FindAsync(id);
-            if (transaction == null)
+            var transactionToDelete = await _context.Transactions.FindAsync(id);
+            if (transactionToDelete == null)
             {
                 return NotFound();
             }
 
-            _context.Transactions.Remove(transaction);
+            _context.Transactions.Remove(transactionToDelete);
             await _context.SaveChangesAsync();
 
             return NoContent();
         }
 
-        private bool TransactionExists(int id)
+        private bool DoesTransactionExists(int id)
         {
             return _context.Transactions.Any(e => e.TransactionId == id);
         }
